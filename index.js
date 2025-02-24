eab25cc4c123f606bb022278863aa62cc2b83319
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2025-02-23 21:49:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const express = require('express');
 const joyasRoutes = require('./routes/joyasRoutes');
 
 const app = express();
-
+const cors = require('cors');
 const port = 3000;
-
-app.use(express.json());
 
 app.use(joyasRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+app.use(cors());
+app.use(express.json());
+app.get("/joyas", async (req, res) => {
+    try {
+        const joyas = await getJoyas();
+        res.json(joyas);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+);
