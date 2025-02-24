6d425729e5ad753aaa4ca7728528bcaffd996ba3
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2025-02-23 21:35:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const joyasRoutes = require('./routes/joyasRoutes');
 
 const app = express();
 
 const port = 3000;
 
 app.use(express.json());
+
+app.use(joyasRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
